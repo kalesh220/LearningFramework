0ceec2a6ff3db6224fa0e30d02872202540aaa9d
KALESWAR <kalidasu220@gmail.com>
2025-12-12 21:31:06 +0530

    modified test.java

--- src/test/java/utility/Test.java
+++ src/test/java/utility/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
        ProviderFromExcel providerFromExcel = new ProviderFromExcel();
        providerFromExcel.getData();
+        System.out.println("kalesh");
     }
 }
